09a3b875949d6ec2ed4223dbbfeadfda3ad86c82
374314125@qq.com <374314125@qq.com>
2023-10-02 19:16:24 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
-  //配置代理跨域
+  //配置代理跨
   devServer: {
     proxy: {
      
