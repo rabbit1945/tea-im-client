02602006f64f45e02fc1aed0124ef8e45452e689
gongzhiyang <374314125@qq.com>
2023-10-12 21:56:32 +0800

    dabao

--- src/router/index.js
+++ src/router/index.js
@@ -81,6 +81,7 @@ router.beforeEach(async (to, from, next) => {
           // 注册 socket
           Vue.use(new VueSocketIO({
             debug: true,
+            // 正式 https://xiaogongtx.com
             // 测试 http://192.168.1.108:9502
             connection: Manager.connect("https://xiaogongtx.com", {
               connectionStateRecovery: {
