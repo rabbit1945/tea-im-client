9c63c8ac2ae6b3d14791d01b2c7dae5defd0e551
gzy <374314125@qq.com>
2023-11-11 19:57:59 +0800

    提交

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
-configureWebpack:{
-	plugins:[
-      new CompressionWebpackPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        // test: /.js$|.html$|.json$|.css/,
-        test: /.js$|.json$|.css/,
-        threshold: 10240, // 只有大小大于该值的资源会被处理
-        minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
-        // deleteOriginalAssets: true // 删除原文件
-      })
-    ]
+{
+  [
+    new CompressionWebpackPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      // test: /.js$|.html$|.json$|.css/,
+      test: /.js$|.json$|.css/,
+      threshold: 10240, // 只有大小大于该值的资源会被处理
+      minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+      // deleteOriginalAssets: true // 删除原文件
+    })
+  ]
 }
 module.exports = defineConfig({
   transpileDependencies: true,
